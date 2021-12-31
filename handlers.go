7bcb5e91bed4954bd02a7c28b416470bbb507761
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-01 02:34:54 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -16,27 +16,28 @@ func (a *application) startHandler(m *tbot.Message) {
 
 // Handle the msg command here
 func (a *application) msgHandler(m *tbot.Message) {
-	cow := `
+	fruw := map[string]string{
+		cow : `
         \   ^__^
          \  (oo)\_______
             (__)\       )\/\
                ||----w |
                ||     ||
 	`
-	pig := `
+		pig : `
         \   <'--'\>______
          \  /. .  ''     \
            ('')  ,        @
             '-._,        /
                )-)_/--( >  jv
               ''''  ''''
-	`
+	`}
 	
 	text := strings.TrimPrefix(m.Text, "cowsay ")
 	lineLen := utf8.RuneCountInString(text) + 2
 	topLine := fmt.Sprintf(" %s ", strings.Repeat("_", lineLen))
 	textLine := fmt.Sprintf("< %s >", text)
 	bottomLine := fmt.Sprintf(" %s ", strings.Repeat("-", lineLen))
-	msg := fmt.Sprintf("```%s\n%s\n%s%s```", topLine, textLine, bottomLine, pig)
+	msg := fmt.Sprintf("```%s\n%s\n%s%s```", topLine, textLine, bottomLine, fruw[pig])
 	a.client.SendMessage(m.Chat.ID, msg, tbot.OptParseModeMarkdown)
 }
