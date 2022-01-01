197fae8c9900f03c89149467766f031257f1cf05
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-01 22:56:11 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -25,7 +25,7 @@ func (a *application) startHandler(m *tbot.Message) {
 	org := ""
 	for i := 0; i < 9; i++{
 		if i < 8{
-			org = org + rndmap[i] + ,}
+			org = org + rndmap[i] + ","}
 	}
 	reply := 
 ` < Если тебе было весело, то это не военное преступление. > 
