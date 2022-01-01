0541fa4a055b51f414a45e0a4184f5d79a290eab
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-01 06:53:27 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -86,10 +86,10 @@ func (a *application) msgHandler(m *tbot.Message) {
 	if fruw[animal] != "" && strings.TrimSpace(m.Text) != animal{
 		reply := fruw[animal]
 		text := strings.TrimPrefix(m.Text, animal + " ")
-		msg = fmt.Sprintf("```< %s > %s ```", text, reply)
+		msg = fmt.Sprintf("```F < %s > %s ```", text, reply)
 	} else { 
 		reply := fruw["pig"]
-		msg = fmt.Sprintf("```< %s > %s ```", m.Text, reply)
+		msg = fmt.Sprintf("```F < %s > %s ```", m.Text, reply)
 	}
 	a.client.SendMessage(m.Chat.ID, msg, tbot.OptParseModeMarkdown)
 }
